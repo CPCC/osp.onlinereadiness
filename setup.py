3c01bfec0c5583bc4db056f4bc160853c61606e0
Julie Sparks <julie.sparks@cpcc.edu>
2013-08-02 11:29:20 -0400

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'onlinereadiness',
-    version = '1.0.2',
+    version = '1.0.3',
     author = 'Central Piedmont Community College',
     description = ('Assessment used to determine student readiness for online classes.'),
     packages = find_packages(),
